7c1cd8e1b6fc19291e8e0511a605742b8a1046c8
Ingo Karkat <swdev@ingo-karkat.de>
2022-04-12 15:41:17 +0200

    Override completion of mountoffer the user's mount points directly while still delivering the original mount options

--- shell/completions.sh
+++ shell/completions.sh
@@ -34,3 +34,29 @@ _chX_complete()
 }
 complete -o filenames -F _chx_complete chx
 complete -o filenames -F _chX_complete chX
+
+
+# mount: Override the default completion to offer the user's mount points
+# directly while still delivering the original mount options.
+_mount_complete()
+{
+    COMPREPLY=()
+    local cur="${COMP_WORDS[COMP_CWORD]}"
+
+    local userMountDirspec="/media/${USER}"
+
+    if [ "${cur#${userMountDirspec}/}" = "$cur" ]; then
+	COMPREPLY=("${userMountDirspec}"/*${cur}${cur:+*})
+	[ "${COMPREPLY[*]}" = "${userMountDirspec}/*${cur}${cur:+*}" ] && COMPREPLY=()   # Emulate shopt -qs nullglob
+    else
+	readarray -t COMPREPLY < <(compgen -d -- "$cur")
+    fi
+
+    if [ ${#COMPREPLY[@]} -gt 0 ]; then
+	readarray -t COMPREPLY < <(printf "%q\n" "${COMPREPLY[@]}")
+	return
+    fi
+
+    "${COMP_DELEGATE_FUNCTION:?}" "${COMP_DELEGATE_COMMAND:?}" "${@:2}"
+}
+completeWithDelegationTo mount -F _mount_complete mount
